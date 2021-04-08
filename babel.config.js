de2cb061fa06f463dfa3795ea916a767ba939432
Matthew Pfister <herrpfister@gmail.com>
2021-04-08 10:21:21 -0500

    updating project to use examples with dev-server

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: [
+            'last 2 Chrome versions',
+            'last 2 Firefox versions',
+            'last 1 Safari versions',
+            'last 1 iOS versions',
+            'last 1 Android version',
+            'last 1 ChromeAndroid version',
+          ],
+        },
+      },
+    ],
     '@babel/preset-typescript',
     '@babel/preset-react',
   ],
